854d28f1a8b45ee4e28b505062fe94658f605852
galloway <digital.locust@gmail.com>
2019-07-29 15:13:50 -0700

    Update parse_files.py

--- parse_files.py
+++ parse_files.py
@@ -1,3 +1,7 @@
+```
+Python script to take data returned from Ansible and extract
+relevant data and combine into two var files for ansible
+```
 import subprocess as sp
 
 cert_file = []
@@ -5,6 +9,7 @@ pem_file = []
 cert_output_file = open("./cert_parse.txt", 'w')
 pem_output_file = open("./pem_parse.txt", 'w')
 
+# Generate list based on directory names
 def generate_file_list(path):
   hostname_list = []
   cmd = "ls " + path
@@ -17,18 +22,19 @@ def generate_file_list(path):
 
 hostnames = generate_file_list("/opt/ansible/wildcard_find/certs")
 
+# Generate Ansible vars file
 for i in hostnames:
   cert_file = f"/opt/ansible/wildcard_find/certs/{i}/var/tmp/{i}_search_cert.txt"
   read_file = open(cert_file, 'r')
-  cert_output_file.write(f"{i} \n")
+  cert_output_file.write(f"{i}: \n")
   for line in read_file:
     if "wildcard" in line:
-      cert_output_file.writelines(line)
+      cert_output_file.writelines(f"  - {line}")
 
 for i in hostnames:
   pem_file = f"/opt/ansible/wildcard_find/certs/{i}/var/tmp/{i}_search_pem.txt"
   read_file = open(pem_file, 'r')
-  pem_output_file.write(f"{i} \n")
+  pem_output_file.write(f"{i}: \n")
   for line in read_file:
     if "wildcard" in line:
-      pem_output_file.writelines(line)
+      pem_output_file.writelines(f"  - {line}")
