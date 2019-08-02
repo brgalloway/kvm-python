5cf378ba4e795640f2f60fd62dd23f5bda9c4cc0
galloway <digital.locust@gmail.com>
2019-08-02 11:37:21 -0700

    converting to oop

--- parse_files.py
+++ parse_files.py
@@ -1,27 +1,29 @@
 import subprocess as sp
 from pathlib import Path
 
-cert_file = []
-pem_file = []
 cert_output_file = open("./cert_parse.txt", 'w')
 pem_output_file = open("./pem_parse.txt", 'w')
 key_output_file = open("./key_parse.txt", 'w')
 
-# Generate list based on directory names
-def generate_file_list(path):
-  hostname_list = []
-  cmd = "ls " + path
-  p = sp.Popen(cmd,shell=True, stdout=sp.PIPE)
+class VarsFile:
+    def __init__(self):
+        self.hostname_list = []
+        self.filenames = []
+    
+    def generate_file_list(self, path):
+        self.cmd = "ls " + path
+        self.p = sp.Popen(self.cmd,shell=True, stdout=sp.PIPE)
 
-  for line in p.stdout:
-    line = line.decode("utf-8")
-    hostname_list.append(line.strip())
-  return hostname_list
+        for line in self.p.stdout:
+            line = line.decode("utf-8")
+            self.hostname_list.append(line.strip())
+        return self.hostname_list
 
-hostnames = generate_file_list("/opt/ansible/wildcard_find/certs")
+varsfile = VarsFile()
+varsfile.generate_file_list("/opt/ansible/wildcard_find/certs")
 
 # Generate Ansible vars file
-for i in hostnames:
+for i in varsfile.hostname_list:
   cert_file = f"/opt/ansible/wildcard_find/certs/{i}/var/tmp/{i}_search_cert.txt"
   read_file = open(cert_file, 'r')
   cert_output_file.write(f"{i}: \n")
@@ -29,7 +31,7 @@ for i in hostnames:
     if "wildcard" in line:
       cert_output_file.writelines(f"  - {line}")
 
-for i in hostnames:
+for i in varsfile.hostname_list:
   pem_file = f"/opt/ansible/wildcard_find/certs/{i}/var/tmp/{i}_search_pem.txt"
   read_file = open(pem_file, 'r')
   pem_output_file.write(f"{i}: \n")
@@ -37,7 +39,7 @@ for i in hostnames:
     if "wildcard" in line:
       pem_output_file.writelines(f"  - {line}")
 
-for i in hostnames:
+for i in varsfile.hostname_list:
   key_file_verify =  Path(f"/opt/ansible/wildcard_find/certs/{i}/var/tmp/{i}_search_key.txt")
   key_file = f"/opt/ansible/wildcard_find/certs/{i}/var/tmp/{i}_search_key.txt"
   if key_file_verify.is_file():
